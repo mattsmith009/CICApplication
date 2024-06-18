4f74fa9c1544e7070ba11cdbf6386f8642af11f3
Matt <msmith26@mit.edu>
2024-06-18 16:56:57 -0600

    Added basic ResearchGate scraper

--- WebScraper/scraper.py
+++ WebScraper/scraper.py
@@ -10,10 +10,4 @@ body_text = body_content.find("div", class_ = "main-content").text
 
 # print(body_text)
 
-# slightly more copmlicated example
-
-URL2 = "https://www.sciencedirect.com/science/article/pii/S1110982323000662"
-page2 = requests.get(URL2)
-soup2 = BeautifulSoup(page2.text, 'html.parser')
-
-print(soup2.text)
+# slightly more complicated example (Needs API Key)
